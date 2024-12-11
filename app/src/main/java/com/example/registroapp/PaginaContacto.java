22d98e5e8294f3128b696c68e03fbff2ed0f8cb2
LoniousSosa <lsosa19@jaumebalmes.net>
2024-12-11 16:18:31 +0100

    Mas activities y fix del theme

---
+++ app/src/main/java/com/example/registroapp/PaginaContacto.java
@@ -0,0 +1,15 @@
+package com.example.registroapp;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PaginaContacto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
