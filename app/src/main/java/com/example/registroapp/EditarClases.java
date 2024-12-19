d66195114bb1443d39b151ac92267512949bb069
karen <karenquispego97@gmail.com>
2024-12-19 13:36:03 +0100

    añadido intent

--- app/src/main/java/com/example/registroapp/EditarClases.java
+++ app/src/main/java/com/example/registroapp/EditarClases.java
@@ -1,6 +1,8 @@
 package com.example.registroapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,21 @@ public class EditarClases extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_editar_clases);
+
+        // Configura los márgenes del diseño principal
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Encuentra el botón por su ID
+        ImageButton imageButtonAñadirClase = findViewById(R.id.imageButtonAñadirClase);
+
+        // Configura el evento de clic para abrir la actividad de información de clase
+        imageButtonAñadirClase.setOnClickListener(view -> {
+            Intent intent = new Intent(EditarClases.this, InformacionClase.class);
+            startActivity(intent);
         });
     }
 }
